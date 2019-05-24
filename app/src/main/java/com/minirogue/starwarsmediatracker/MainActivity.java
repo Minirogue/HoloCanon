a51dd67962ee5e4a583a39a55d9913d44a17869e
minirogue <ettiwsl@gmail.com>
2019-05-24 13:39:48 -0700

    move TODOs to local AndroidStudio tasks

--- app/src/main/java/com/minirogue/starwarsmediatracker/MainActivity.java
+++ app/src/main/java/com/minirogue/starwarsmediatracker/MainActivity.java
@@ -1,16 +1,5 @@
 package com.minirogue.starwarsmediatracker;
 
-/* TODO Sorting
-*  TODO Update database
-*  TODO revise media_list_item.xml
-*  TODO properly implement "view media" pages
-*  TODO add menus
-*  TODO add options to permanently filter out certain types of media (junior, etc.)
-*  TODO add options to view collected comics vs individual comics
-*  TODO add options for filtering out redundant material (audiobooks, comic adaptations (e.g. Thrawn), novelizations)
- */
-
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
