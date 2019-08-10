b849f2e4b511db508efdec3dd0a8b9a3892c1e81
minirogue <ettiwsl@gmail.com>
2019-08-09 23:29:15 -0700

    version 1.0.3: add v1.0.3 update text, add twitter/discord links

--- app/src/main/java/com/minirogue/starwarscanontracker/EntryFragment.kt
+++ app/src/main/java/com/minirogue/starwarscanontracker/EntryFragment.kt
@@ -2,16 +2,20 @@ package com.minirogue.starwarscanontracker
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_entry.view.*
 
 class EntryFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_entry, container, false)
+        val fragView = inflater.inflate(R.layout.fragment_entry, container, false)
+        fragView.welcome_textview.movementMethod = LinkMovementMethod.getInstance()
+        return fragView
     }
 
 
