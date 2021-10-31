31b7ccdf538ba8e503bd7b234a8c326f6af153c6
Shawn Witte <ettiwsl@gmail.com>
2021-10-31 14:35:54 -0700

    attempt to fix crash on reopening app (#177)
    * move creation of AsyncTask object so it doesn't get executed twice

--- usecase/src/main/java/com/minirogue/usecase/UpdateMediaDatabaseUseCase.kt
+++ usecase/src/main/java/com/minirogue/usecase/UpdateMediaDatabaseUseCase.kt
@@ -1,10 +1,16 @@
 package com.minirogue.usecase
 
 import com.minirogue.starwarscanontracker.core.model.CSVImporter
+import com.minirogue.starwarscanontracker.core.model.FilterUpdater
+import android.app.Application
 import javax.inject.Inject
 
-public class UpdateMediaDatabaseUseCase @Inject constructor(private val csvImporter: CSVImporter) {
+public class UpdateMediaDatabaseUseCase @Inject constructor(
+    private val application: Application,
+    private val filterUpdater: FilterUpdater,
+) {
     operator fun invoke(forced: Boolean = false) {
+        val csvImporter = CSVImporter(application, filterUpdater)
         csvImporter.execute(forced)
     }
 }
