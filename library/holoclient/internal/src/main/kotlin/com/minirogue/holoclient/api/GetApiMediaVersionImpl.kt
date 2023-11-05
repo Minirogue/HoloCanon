25f460ff8f3d6e8a63784f96796a0877d59ef6db
Minirogue <shawn.l.witte@gmail.com>
2023-11-05 14:42:43 -0800

    update versions to latest compatible compose/kotlin versions and fix lint issue

--- library/holoclient/internal/src/main/kotlin/com/minirogue/holoclient/api/GetApiMediaVersionImpl.kt
+++ library/holoclient/internal/src/main/kotlin/com/minirogue/holoclient/api/GetApiMediaVersionImpl.kt
@@ -3,18 +3,18 @@ package com.minirogue.holoclient.api
 import android.util.Log
 import com.minirogue.holoclient.GetApiMediaVersion
 import com.minirogue.starwarscanontracker.core.result.HoloResult
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.engine.okhttp.*
-import io.ktor.client.plugins.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.request.*
-import io.ktor.serialization.kotlinx.json.*
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.ResponseException
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.SerializationException
 import java.io.IOException
 import javax.inject.Inject
 
-private const val TAG = "GetMediaApiMediaVersionImpl"
+private const val TAG = "GetMediaApiMediaVersion"
 class GetApiMediaVersionImpl @Inject constructor() : GetApiMediaVersion {
     override suspend fun invoke(): HoloResult<Int> {
         return HttpClient(OkHttp) {
