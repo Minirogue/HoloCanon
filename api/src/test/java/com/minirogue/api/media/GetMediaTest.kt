cff2faded57e89dac255ccd56b0d52cac032dbae
Shawn Witte <shawn.l.witte@gmail.com>
2024-02-21 00:23:22 -0800

    minor database maintenance (#378)
    * add null image test, update a few database items, add function to surface broken image links

--- api/src/test/java/com/minirogue/api/media/GetMediaTest.kt
+++ api/src/test/java/com/minirogue/api/media/GetMediaTest.kt
@@ -1,11 +1,30 @@
 package com.minirogue.api.media
 
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class GetMediaTest {
     @Test
-    fun `ensure all media can be properly read from the CSV`() {
-        runBlocking { getFullMediaList() }
+    fun `ensure all media can be properly read from the CSV`() = runTest {
+        // Arrange
+
+        // Act
+        getFullMediaList()
+
+        // Assert
+        // Act step shouldn't fail
+    }
+
+    @Test
+    fun `ensure all media has cover image links`() = runTest {
+        // Arrange
+        val imageAddresses = getFullMediaList().associate { it.id to it.imageUrl }
+
+        // Act
+        val missingImages: Set<Long> = imageAddresses.filterValues { it == null }.keys
+
+        // Assert
+        assertTrue("The following ids are missing images: $missingImages", missingImages.isEmpty())
     }
 }
