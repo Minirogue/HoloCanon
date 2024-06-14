0e04bbc456b6031d90a1a8909e6e3ed882dff1af
Minirogue <shawn.l.witte@gmail.com>
2024-06-13 22:51:59 -0700

    remove extra file from merge issue

--- library/serialization-ext/internal/src/commonMain/kotlin/com/holocanon/library/serialization/ext/internal/Json.kt
+++
@@ -1,9 +0,0 @@
-package com.holocanon.library.serialization.ext.internal
-
-import kotlinx.serialization.json.Json
-
-val holocanonJson = Json {
-    ignoreUnknownKeys = true
-    prettyPrint = true
-    isLenient = true
-}
