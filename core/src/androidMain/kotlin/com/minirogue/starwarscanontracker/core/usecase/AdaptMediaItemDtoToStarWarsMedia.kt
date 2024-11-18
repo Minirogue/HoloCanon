f634fdf891efced18b644c965507b322562de177
Shawn Witte <shawn.l.witte@gmail.com>
2024-11-17 20:40:49 -0800

    Turn flows into hot flows to speed up media list query (#526)
    Use ShareIn on Flows used in AdaptMediaItemDtoToStarWarsMedia so they are not rerun every time they are referenced (creating hundreds/thousands of unnecessary database queries)

--- core/src/androidMain/kotlin/com/minirogue/starwarscanontracker/core/usecase/AdaptMediaItemDtoToStarWarsMedia.kt
+++ core/src/androidMain/kotlin/com/minirogue/starwarscanontracker/core/usecase/AdaptMediaItemDtoToStarWarsMedia.kt
@@ -1,6 +1,7 @@
 package com.minirogue.starwarscanontracker.core.usecase
 
 import android.util.Log
+import com.holocanon.library.coroutine.ext.HolocanonDispatchers
 import com.minirogue.common.model.Company
 import com.minirogue.common.model.MediaType
 import com.minirogue.common.model.StarWarsMedia
@@ -8,9 +9,13 @@ import com.minirogue.starwarscanontracker.core.model.room.dao.DaoCompany
 import com.minirogue.starwarscanontracker.core.model.room.dao.DaoSeries
 import com.minirogue.starwarscanontracker.core.model.room.entity.MediaItemDto
 import dagger.Reusable
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.shareIn
 import kotlinx.serialization.json.Json
 import javax.inject.Inject
 
@@ -19,20 +24,25 @@ class AdaptMediaItemDtoToStarWarsMedia @Inject internal constructor(
     private val daoCompany: DaoCompany,
     private val daoSeries: DaoSeries,
     private val json: Json,
+    private val dispatchers: HolocanonDispatchers,
 ) {
-    private var companyMap: Flow<Map<Int, Company>> = daoCompany.getAllCompanies()
+    private val adapterScope = CoroutineScope(Job() + dispatchers.default)
+    private val companyMap: SharedFlow<Map<Int, Company>> = daoCompany.getAllCompanies()
         .map { list ->
             list.associate {
                 it.id to json.decodeFromString<Company>("\"${it.companyName}\"")
             }
         }
-    private var seriesMap: Flow<Map<Int, String>> = daoSeries.getAllSeries()
+        .shareIn(scope = adapterScope, started = SharingStarted.Lazily, replay = 1)
+
+    private val seriesMap: SharedFlow<Map<Int, String>> = daoSeries.getAllSeries()
         .map { list -> list.associate { it.id to it.title } }
+        .shareIn(scope = adapterScope, started = SharingStarted.Lazily, replay = 1)
 
     suspend operator fun invoke(mediaItemDto: MediaItemDto): StarWarsMedia = StarWarsMedia(
         id = mediaItemDto.id.toLong(),
         title = mediaItemDto.title,
-        type = MediaType.getFromLegacyId(mediaItemDto.type) ?: MediaType.REFERENCE,
+        type = MediaType.getFromLegacyId(mediaItemDto.type),
         imageUrl = mediaItemDto.imageURL,
         releaseDate = mediaItemDto.date,
         timeline = mediaItemDto.timeline.toFloat(),
