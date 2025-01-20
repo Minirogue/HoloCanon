5b80bf5aa806616144ee8178bcd2d86349ec04cf
Minirogue <shawn.l.witte@gmail.com>
2025-01-19 17:26:23 -0800

    Update for compatibility with plugin 0.0.4

--- build-logic/convention/src/main/kotlin/convention/KotlinSymbolProcessingConfiguration.kt
+++
@@ -1,7 +0,0 @@
-package convention
-
-import org.gradle.api.plugins.PluginManager
-
-fun PluginManager.applyKsp() {
-    apply("com.google.devtools.ksp")
-}
--- build-logic/convention/src/main/kotlin/ext/ProjectExt.kt
+++
@@ -1,5 +0,0 @@
-package ext
-
-import org.gradle.api.Project
-
-fun Project.isMultiplatform(): Boolean = plugins.hasPlugin("org.jetbrains.kotlin.multiplatform")
