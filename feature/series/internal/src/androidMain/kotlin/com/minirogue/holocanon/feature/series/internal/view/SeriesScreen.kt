ccf2314c0735b7c2307975763bbd32e54c0de24c
shwitte <ettiwsl@gmail.com>
2025-05-26 17:46:08 -0700

    Move filter and database update from MainActivity to application start. Migrate to assisted injection for media item and series screens

--- feature/series/internal/src/androidMain/kotlin/com/minirogue/holocanon/feature/series/internal/view/SeriesScreen.kt
+++ feature/series/internal/src/androidMain/kotlin/com/minirogue/holocanon/feature/series/internal/view/SeriesScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
@@ -31,9 +30,12 @@ internal fun SeriesScreen(
     modifier: Modifier = Modifier,
     seriesName: String,
     navController: NavController,
-    viewModel: SeriesViewModel = hiltViewModel(), // TODO assistedInject
+    viewModel: SeriesViewModel = hiltViewModel(
+        creationCallback = { factory: SeriesViewModel.Factory ->
+            factory.create(seriesName)
+        },
+    ),
 ) {
-    LaunchedEffect(seriesName) { viewModel.setSeries(seriesName) }
     val state by viewModel.state.collectAsStateWithLifecycle()
 
     Column(modifier = modifier.fillMaxSize()) {
@@ -62,10 +64,13 @@ internal fun SeriesScreen(
             media = state.mediaAndNotes,
             onItemClicked = { navController.navigate(MediaItemNav(it)) },
         )
-        state.checkboxSettings?.also {
+        val seriesId = state.seriesId
+        val checkboxSettings = state.checkboxSettings
+        if (seriesId != null && checkboxSettings != null) {
             CheckboxGroup(
+                seriesId = seriesId,
                 mediaNotes = state.seriesNotes,
-                checkboxSettings = it,
+                checkboxSettings = checkboxSettings,
                 onBox1Clicked = viewModel::toggleCheckbox1,
                 onBox2Clicked = viewModel::toggleCheckbox2,
                 onBox3Clicked = viewModel::toggleCheckbox3,
@@ -93,11 +98,12 @@ private fun SeriesList(
 @Composable
 private fun CheckboxGroup(
     modifier: Modifier = Modifier,
+    seriesId: Int,
     mediaNotes: MediaNotes?,
     checkboxSettings: CheckboxSettings,
-    onBox1Clicked: (newValue: Boolean) -> Unit,
-    onBox2Clicked: (newValue: Boolean) -> Unit,
-    onBox3Clicked: (newValue: Boolean) -> Unit,
+    onBox1Clicked: (seriesId: Int, newValue: Boolean) -> Unit,
+    onBox2Clicked: (seriesId: Int, newValue: Boolean) -> Unit,
+    onBox3Clicked: (seriesId: Int, newValue: Boolean) -> Unit,
 ) = Column(modifier = modifier) {
     val isBox1Checked = mediaNotes?.isBox1Checked
     val isBox2Checked = mediaNotes?.isBox2Checked
@@ -106,19 +112,19 @@ private fun CheckboxGroup(
         TextAndCheckbox(
             checkboxSettings.checkbox1Setting.name,
             isBox1Checked,
-        ) { onBox1Clicked(!isBox1Checked) }
+        ) { onBox1Clicked(seriesId, !isBox1Checked) }
     }
     if (checkboxSettings.checkbox2Setting.isInUse && isBox2Checked != null) {
         TextAndCheckbox(
             checkboxSettings.checkbox2Setting.name,
             isBox2Checked,
-        ) { onBox2Clicked(!isBox2Checked) }
+        ) { onBox2Clicked(seriesId, !isBox2Checked) }
     }
     if (checkboxSettings.checkbox3Setting.isInUse && isBox3Checked != null) {
         TextAndCheckbox(
             checkboxSettings.checkbox3Setting.name,
             isBox3Checked,
-        ) { onBox3Clicked(!isBox3Checked) }
+        ) { onBox3Clicked(seriesId, !isBox3Checked) }
     }
 }
 
--- library/coroutine-ext/internal/src/commonMain/kotlin/com/holocanon/library/coroutine/ext/internal/ApplicationScopeImpl.kt
+++ library/coroutine-ext/internal/src/commonMain/kotlin/com/holocanon/library/coroutine/ext/internal/ApplicationScopeImpl.kt
@@ -2,9 +2,8 @@ package com.holocanon.library.coroutine.ext.internal
 
 import com.holocanon.library.coroutine.ext.ApplicationScope
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
 
 internal class ApplicationScopeImpl :
     ApplicationScope,
-    CoroutineScope by CoroutineScope(Job() + Dispatchers.Main)
+    CoroutineScope by MainScope()
