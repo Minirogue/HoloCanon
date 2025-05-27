437b6950f304591fb8b8e1029675fdf39e33c1f8
shwitte <ettiwsl@gmail.com>
2025-05-26 19:58:23 -0700

    remove unused dependencies

--- app/src/main/kotlin/com/minirogue/starwarscanontracker/application/CanonTrackerApplication.kt
+++ app/src/main/kotlin/com/minirogue/starwarscanontracker/application/CanonTrackerApplication.kt
@@ -1,12 +1,7 @@
 package com.minirogue.starwarscanontracker.application
 
 import android.app.Application
-import coil.ImageLoader
-import coil.ImageLoaderFactory
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CanonTrackerApplication : Application(), ImageLoaderFactory {
-    override fun newImageLoader(): ImageLoader = ImageLoader.Builder(applicationContext)
-        .build()
-}
+class CanonTrackerApplication : Application()
