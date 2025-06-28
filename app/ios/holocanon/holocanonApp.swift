442d47bcf08e8b111216f0a812e41d553df110d3
shwitte <ettiwsl@gmail.com>
2025-06-28 14:53:31 -0700

    remove submodule

---
+++ app/ios/holocanon/holocanonApp.swift
@@ -0,0 +1,17 @@
+//
+//  holocanonApp.swift
+//  holocanon
+//
+//  Created by Shawn Witte on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct holocanonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ app/ios/holocanonTests/holocanonTests.swift
@@ -0,0 +1,17 @@
+//
+//  holocanonTests.swift
+//  holocanonTests
+//
+//  Created by Shawn Witte on 6/6/25.
+//
+
+import Testing
+@testable import holocanon
+
+struct holocanonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
