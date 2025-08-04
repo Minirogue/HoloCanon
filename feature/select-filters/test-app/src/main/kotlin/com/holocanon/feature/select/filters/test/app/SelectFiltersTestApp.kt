5bb6a606426949d9fe2a090bbc93ccad874244c7
Shawn Witte <shawn.l.witte@gmail.com>
2025-08-03 17:00:42 -0700

    fix test app (#646)
    * fix test app
    
    * bump build logic to 0.1.1

---
+++ feature/select-filters/test-app/src/main/kotlin/com/holocanon/feature/select/filters/test/app/SelectFiltersTestApp.kt
@@ -0,0 +1,17 @@
+package com.holocanon.feature.select.filters.test.app
+
+import android.app.Application
+import com.minirogue.feature.test.app.TestAppDi
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.DependencyGraph
+import dev.zacsweers.metro.createGraph
+
+class SelectFiltersTestApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        TestAppDi.setInstance(createGraph<SelectFiltersTestAppDi>().createTestAppDi())
+    }
+}
+
+@DependencyGraph(scope = AppScope::class, isExtendable = true)
+interface SelectFiltersTestAppDi
