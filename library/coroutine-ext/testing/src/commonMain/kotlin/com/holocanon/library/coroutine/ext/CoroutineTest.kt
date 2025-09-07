a3037ad6d56ff10814901ce362edde672a8c4cd3
Shawn Witte <shawn.l.witte@gmail.com>
2025-09-06 19:57:30 -0700

    upgrade gradle wrapper, upgrade build logic, and fix some warnings (#656)

--- library/coroutine-ext/testing/src/commonMain/kotlin/com/holocanon/library/coroutine/ext/CoroutineTest.kt
+++ library/coroutine-ext/testing/src/commonMain/kotlin/com/holocanon/library/coroutine/ext/CoroutineTest.kt
@@ -1,12 +1,14 @@
 package com.holocanon.library.coroutine.ext
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 
+@OptIn(ExperimentalCoroutinesApi::class)
 interface CoroutineTest {
     @BeforeTest
     fun setMainDispatcher() {
